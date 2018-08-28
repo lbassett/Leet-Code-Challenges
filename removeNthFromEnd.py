42b47f7566a8f071f606f16336e3b1d7a4f10327
lbassett <liam.f.bassett@gmail.com>
2018-08-28 16:12:03 -0400

    removeNthFromEnd

---
+++ removeNthFromEnd.py
@@ -0,0 +1,34 @@
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# Challenge: Remove the nth element from the end of a linked list in only one pass through
+
+def removeNthFromEnd(self, head, n):
+    """
+    :type head: ListNode
+    :type n: int
+    :rtype: ListNode
+    """
+    headnode = head
+    
+    tailnode = head
+    
+    counter = 0
+    
+    while counter <n:
+        tailnode = tailnode.next
+        counter += 1
+    
+    if tailnode == None:
+        return(headnode.next)
+    else:
+        while tailnode.next != None:
+            tailnode = tailnode.next
+            headnode = headnode.next
+    
+        headnode.next = headnode.next.next
+        return(head)

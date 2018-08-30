2e291ff48b80bdc86158cc4bf6120376dbcc932f
lbassett <liam.f.bassett@gmail.com>
2018-08-29 23:12:50 -0400

    JumpGame

---
+++ JumpGame.py
@@ -0,0 +1,26 @@
+# Challenge: Jump Game
+# Given an array of non-negative integers, you are initially positioned at the first index of the array.
+# Each element in the array represents your maximum jump length at that position.
+# Determine if you are able to reach the last index.
+
+
+
+def canJump(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    
+    index = 0
+    
+    furthest = 0
+    
+    leng = len(nums)
+    
+    while index <= furthest and index<leng:
+        val = nums[index]
+        f = index + val
+        furthest = max(furthest,f)
+        index+=1
+    
+    return(index == leng)

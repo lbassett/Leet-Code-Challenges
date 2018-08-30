f5f5054f356241a13b5efac7ce96db540d0dc99b
lbassett <liam.f.bassett@gmail.com>
2018-08-30 14:16:20 -0400

    Spiral Order

---
+++ SpiralOrder.py
@@ -0,0 +1,44 @@
+# Challenge: Spiral matrix
+# Given an mxn matrix, return a list of the elements going clockwise in
+
+def spiralOrder(self, matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: List[int]
+    """
+    if matrix == []:
+        return([])
+    else:
+        return(self.recursSpiralOrder([], matrix))
+    
+def recursSpiralOrder(self, previous, matrix):
+    if len(matrix) == 1:
+        retlist = previous + matrix[0]
+        return(retlist)
+    elif len(matrix) == 2:
+        bottom = matrix[1]
+        bottom.reverse()
+        retlist = previous + matrix[0] + bottom
+        return(retlist)
+    elif len(matrix[0]) == 1:
+        column = []
+        for layer in matrix:
+            column += layer
+        retlist = previous + column
+        return(retlist)
+    elif len(matrix[0]) == 0:
+        return(previous)
+    else:
+        top = matrix[0]
+        bottom = matrix[-1]
+        bottom.reverse()
+        right = []
+        left = []
+        newmat = matrix[1:-1]
+        newermat = []
+        for layer in newmat:
+            right += [layer[-1]]
+            left = [layer[0]] + left
+            newermat += [layer[1:-1]]
+        outerlist = previous + top + right + bottom + left
+        return(self.recursSpiralOrder(outerlist, newermat)) # Tail-Recursion

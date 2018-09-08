5b0a67cc33da42bfc2fc112f915f18d448405202
lbassett <liam.f.bassett@gmail.com>
2018-09-07 23:57:16 -0400

    MergeTwoSortedLists.py

---
+++ MergeTwoSortedLists.py
@@ -0,0 +1,27 @@
+# Challenge: Merge Two sorted linked lists
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def mergeTwoLists(self, l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    
+    if l1 == None:
+        return(l2)
+    elif l2 == None:
+        return(l1)
+    else:
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return(l1)
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return(l2)

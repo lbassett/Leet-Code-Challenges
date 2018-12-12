45561dcf41fed916df5f59bfd9e2f808be3bc47b
lbassett <liam.f.bassett@gmail.com>
2018-12-12 18:32:13 -0500

    FirstMissingPositive.py

---
+++ FirstMissingPositive.py
@@ -0,0 +1,25 @@
+# Challenge: First Missing Positive:
+
+# Given an unordered unique list of integers, return the smallest positive integer that isn't present
+
+def firstMissingPositive(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    
+    summ = 0
+    
+    for x in nums:
+        if x > 0:
+            summ += 10 ** (x-1)
+    
+    posint = 1
+    
+    while summ % 10 != 0:
+        summ = summ // 10
+        posint += 1
+    
+    
+    return(posint)
+
